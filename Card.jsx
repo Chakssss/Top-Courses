76c482280c8e6839c0fd7450de4e7c1812eea62d
Chakshu Gautam <121420937+Chakssss@users.noreply.github.com>
2024-04-30 21:49:35 +0530

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Card = (props) => {
+  let course = props.course;
+  let likedCourses = props.likedCourses;
+  let setLikedCourses = props.setLikedCourses;
+
+  return (
+    <div className="w-[300px] bg-bgDark bg-opacity-80 rounded-md overflow-hidden">
+      <div className="relative">
+        <img src={course.image.url} />
+      </div>
+
+      <div className="p-4">
+        <p className="text-white font-semibold text-lg leading-6">
+          {course.title}
+        </p>
+        <p className="mt-2 text-white">
+          {course.description.length > 100
+            ? course.description.substr(0, 100) + "..."
+            : course.description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
